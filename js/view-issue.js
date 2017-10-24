ee50d810dd062f1e80cf3c2ad6a11a92ffb1c323
Tom Snelling <tomsnelling8@gmail.com>
2017-10-24 17:40:58 +0100

    issue tabs now show active issue

--- js/view-issue.js
+++ js/view-issue.js
@@ -1,3 +1,20 @@
+function getParameterByName(name, url) {
+	if (!url) url = window.location.href;
+	name = name.replace(/[\[\]]/g, "\\$&");
+	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+		results = regex.exec(url);
+	if (!results) return null;
+	if (!results[2]) return '';
+	return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+$(document).ready(function() {
+	setTimeout(function() {
+		var id = '#issue-link-' + getParameterByName('id');
+		$(id).addClass('active');
+	}, 50);
+});
+
 $(document).on('click', '#button-edit', function() {
 	$('input[type="text"], textarea').each(function(i, el) {
 		$(el).prop('disabled', function(i, v) { return !v; });
