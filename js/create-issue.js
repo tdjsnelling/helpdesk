3cd49c88c7aade3997a3c122665f43c9da1b2f8e
tdjsnelling <tomsnelling8@gmail.com>
2017-10-30 14:39:44 +0000

    add ticket created prompt

--- js/create-issue.js
+++ js/create-issue.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
 	$('#created-by').text(localStorage.getItem('currentUser'));
 });
+
+$(document).on('click', '#button-create', function() {
+	$('#created-prompt').fadeIn(500, () => {
+		setTimeout(() => {
+			$('#created-prompt').fadeOut(500);
+		}, 2000);
+	});
+});
